1b0cff78285913137d45ef675bc9649177dfd8fb
Nguyen Nhu Y <nhuynny883@gmail.com>
2017-09-16 08:42:20 +0000

    firts commit

---
+++ db/migrate/20170916072705_create_ideas.rb
@@ -0,0 +1,12 @@
+class CreateIdeas < ActiveRecord::Migration[5.1]
+  def change
+    create_table :ideas do |t|
+      t.string :name
+      t.text :description
+      t.string :picture
+      t.string :birthday
+
+      t.timestamps
+    end
+  end
+end
